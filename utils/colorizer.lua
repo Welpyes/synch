f9bfa65d165859a17d25706ab07c02a15f864620
Welpyes <helloilikeoppai@protonmail.com>
2026-04-22 15:33:57 +0800

    feat: add colorizer utility with ANSI and Hex support

---
+++ utils/colorizer.lua
@@ -0,0 +1,33 @@
+local Colorizer = {}
+
+local ansi_colors = {
+  black   = 30,
+  red     = 31,
+  green   = 32,
+  yellow  = 33,
+  blue    = 34,
+  magenta = 35,
+  cyan    = 36,
+  white   = 37,
+  reset   = 0,
+}
+
+local function hex_to_rgb(hex)
+  hex = hex:gsub("#", "")
+  return tonumber("0x" .. hex:sub(1, 2)), tonumber("0x" .. hex:sub(3, 4)), tonumber("0x" .. hex:sub(5, 6))
+end
+
+function Colorizer.colorize(text, color)
+  if not color then return text end
+
+  if ansi_colors[color] then
+    return string.format("\27[%dm%s\27[0m", ansi_colors[color], text)
+  elseif color:match("^#%x%x%x%x%x%x$") then
+    local r, g, b = hex_to_rgb(color)
+    return string.format("\27[38;2;%d;%d;%dm%s\27[0m", r, g, b, text)
+  end
+
+  return text
+end
+
+return Colorizer
